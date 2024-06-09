b5a92cf82958c3fa690618ba1ac18fa72c6ed7f8
ren <r999b66k222@outlook.jp>
2024-06-09 15:50:13 +0900

    feat: Question/q1

--- src/UseAsyncAwait/js/q1.js
+++ src/UseAsyncAwait/js/q1.js
@@ -5,7 +5,7 @@ const excuteButton = document.querySelector('.excute');
 
 excuteButton.addEventListener('click', (event) => {
 
-  const fetchData = _____() => {
+  const fetchData = async() => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve("ここが２番目に実行される。(3秒後)");
